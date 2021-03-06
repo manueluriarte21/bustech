3358b795f00e192c8643581a2f9d5f49a73e329f
manueluriarte21 <80177093+manueluriarte21@users.noreply.github.com>
2021-03-06 19:15:19 -0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.menu-btn').addEventListener ('click', () =>{
-     document.querySelector('.nav-menu').classList.toggle('show');
-})
-
-
-ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.objetivos', {delay: 900});
-ScrollReveal().reveal('.card-banner-one', {delay: 900});
